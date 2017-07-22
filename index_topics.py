c3b6f42b88fd74299b13799a48f84d678a3dc662
vendi <svitlana.vakulenko@gmail.com>
2017-07-23 00:53:50 +0200

    new: topics dump to ES

--- index_topics.py
+++ index_topics.py
@@ -8,39 +8,52 @@ svakulenko
 import json
 import string
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
 from settings import *
 from mappings import *
 
 
-class ESClient():
-    def __init__(self, index):
-        self.es = Elasticsearch()
-        self.index = index
-
-    def load_topics(self, topics_json):
-        # delete previous index
-        self.es.indices.delete(index=self.index, ignore=[400, 404])
-        # define mapping
-        self.es.indices.create(index=self.index, body=create_index_body)
-
-        for topic in topics_json:
-
-            title = topic['title']
-            description = topic['description']
-            narrative = topic['narrative']
-            self.es.index(index=self.index, doc_type='topics', id=topic['topid'],
-                          body={'title': title,
-                                'description': description,
-                                'narrative': narrative,
-                                })
+es = Elasticsearch()
 
 
-# load topics into ES index
+def tokenize_in_es(text, index_name=INDEX):
+    tokens = es.indices.analyze(index=index_name, analyzer='english', text=text)
+    return [token['token'] for token in tokens['tokens']]
+
+
+ # json-ize the lines in the file
+def make_documents(f, index_name):
+    topics_json = json.load(f)
+    for topic in topics_json:
+
+        title = topic['title']
+        title_terms = tokenize_in_es(title)
+
+        doc = {
+                '_op_type': 'index',
+                '_index': INDEX,
+                '_type': 'topics',
+                '_id': topic['topid'],
+                '_source': {'title': title,
+                            'title_terms': title_terms,
+                            'description': topic['description'],
+                            'description': topic['description'],
+                            'narrative': topic['narrative'],
+                            }
+        }
+        yield( doc )
+        
+
+
+
+# load topics into ES index in bulk
 def load_topics_in_ES(file=TOPICS, index_name=INDEX):
-    topics_json = json.load(open(file))
-    es = ESClient(index=index_name)
-    es.load_topics(topics_json)
+    # es.indices.delete(index=index_name)
+    es.indices.create(index=index_name, body=create_index_body)
+
+    with open(file, "r") as f:
+        bulk(es, make_documents(f, index_name))
 
 
 if __name__ == '__main__':
