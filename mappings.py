77d46cf282955c70973eda5ed6ac500d90d90ccc
vendi <svitlana.vakulenko@gmail.com>
2017-07-23 00:54:25 +0200

    fix: mappings with english analyzer

--- mappings.py
+++ mappings.py
@@ -15,3 +15,103 @@ mapping_exact_title = '''
     }
   }
 }'''
+
+mapping_td = '''
+{  
+  "mappings":{  
+    "topics":{  
+      "properties":{  
+        "td": {
+          "type": string,
+          "analyzer": english
+        },
+      }
+    }
+  }
+}'''
+
+english_3 = {
+  "settings": {
+    "analysis": {
+      "filter": {
+        "custom_english_stemmer": {
+          "type": "stemmer",
+          "name": "english"
+        }
+      },
+      "analyzer": {
+        "custom_lowercase_stemmed": {
+          "tokenizer": "standard",
+          "stopwords": [ "and", "the", "of" ],
+          "filter": [
+            "lowercase",
+            "custom_english_stemmer"
+          ]
+        }
+      }
+    }
+  },
+  "mappings": {
+    "topics": {
+      "properties": {
+        "title": {
+          "type": "string",
+          "analyzer": "custom_lowercase_stemmed"
+        },
+        "description": {
+          "type": "string",
+          "analyzer": "custom_lowercase_stemmed"
+        },
+        "narrative": {
+          "type": "string",
+          "analyzer": "custom_lowercase_stemmed"
+        }
+      }
+    }
+  }
+}
+
+create_index_body = {
+      'settings': {
+        # just one shard, no replicas for testing
+        'number_of_shards': 1,
+        'number_of_replicas': 0,
+      },
+      'mappings': {
+        'topics': {
+          'properties': {
+            'title': {'type': 'text', 'analyzer': 'english'},
+            'description': {'type': 'text', 'analyzer': 'english'},
+            'narrative': {'type': 'text', 'analyzer': 'english'}
+          }
+        },
+        'tweets': {
+          'properties': {
+            'tweet': {'type': 'text', 'analyzer': 'english'},
+          }
+        }
+      }
+    }
+
+
+
+mapping_keyword_title = '''
+{  
+  "mappings":{  
+    "topics":{  
+      "properties":{  
+        "title":{  
+          "type":"keyword"
+        },
+        "description": {
+          "type": string,
+          "analyzer": english
+        },
+        "narrative": {
+          "type": string,
+          "analyzer": english
+        },
+      }
+    }
+  }
+}'''
