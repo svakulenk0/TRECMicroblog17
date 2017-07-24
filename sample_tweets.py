a031a3ba5d4486eb302494365fc4040a6a8317b8
vendi <svitlana.vakulenko@gmail.com>
2017-07-24 09:26:59 +0200

    new: sample tweets annotated with relevance

---
+++ sample_tweets.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+23 July 2017
+svakulenko
+
+Positive and negative samples of relevant tweets
+'''
+
+TRUE = [
+        "Im ordering pizza at Panera Bread",
+        "justin bieber will have a concert next tuesday",
+        "Someone give me 2 tickets to the justin bieber concert in Arlington,Tx so me and my mom can go"
+        ]
+
+FALSE = [
+        "The Iris Tank One Piece Swimsuit is a classic statement one piece, perfect for every beach girl. The features of... http://fb.me/1wXYqZnKy",
+        "luktianashawty justin bieber is my pick for https://t.co/pcjuxjgigc fan army #beliebers https://t.co/tqqa36sxdq",
+        "cun_taynara rt @justinbie8er94: rt to vote #mtvhottest justin bieber https://t.co/u7v6b5gibz Justin Bieber",
+        '''uoiae ＠llililill  july 23, 2017 at 06:25amjuly 23, 2017 at 06:25amjuly 23, 2017 at 06:25amjuly 23, 2017 at 06:25amjuly 23, 2017 at 06:25amjuly …''',
+        "••Great Full Time Benefits at General Security Services Corporation!•• http://JobGuideND.com ",
+        "Power supply, water, hiked prices in Unilag made #unilag11 lead a #protest. They were rusticated #OccupyUnilag",
+        "ಇಂಡಿಯನ್ politicians n border security force also have their good share..."
+        ]
