994685adee21f185a62b845474d3d319f39715fb
vendi <svitlana.vakulenko@gmail.com>
2017-08-08 21:30:29 +0200

    new: monitoring tweets relevant for leopoldstadt and Stuwerviertel

---
+++ client_communidata.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+'''
+7 Aug 2017
+svakulenko
+
+Twitter stream topic matcher via ElasticSearch expanded with search snippets from Google Custom Search API
+max score threshold
+Search Google snippets
+'''
+import requests
+import re
+
+from tweepy.streaming import StreamListener
+from tweepy import Stream, API, OAuthHandler
+
+from elasticsearch import Elasticsearch
+
+from settings import *
+from sample_tweets import TRUE, FALSE
+# 2.1
+THRESHOLD = 2
+
+INDEX = 'communidata'
+
+# set up Twitter connection
+auth_handler = OAuthHandler(APP_KEY, APP_SECRET)
+auth_handler.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitter_client = API(auth_handler)
+
+# set up ES connection
+es = Elasticsearch()
+
+
+multi = {
+            "query": {
+                "multi_match" : {
+                    "type": "most_fields",
+                    "fields": ['search_snippets', 'keywords', 'wiki_title', 'wiki_summary', 'wiki_content']
+                }
+            }
+        }
+
+
+def tokenize_in_es(text, index=INDEX):
+    '''
+    Produce tokens from text via ES english analyzer
+    '''
+    tokens = es.indices.analyze(index=index, analyzer='english', text=text)
+    return [token['token'] for token in tokens['tokens']]
+
+
+def search_all(query, threshold=THRESHOLD, explain=False, index=INDEX):
+    '''
+    Search tweet through topics in ES index
+    '''
+    # search in all 3 facets of the topic with equal weights,
+    request = multi
+    request['query']['multi_match']['query'] = query
+    results = es.search(index=index, body=request, doc_type='topic', explain=explain)['hits']
+    # filter out the scores below the specified threshold
+    if results['max_score']:
+        if results['max_score'] > threshold:
+            topic = results['hits'][0]
+            # topic title terms have to be subset of the tweet
+            # title_terms = set(topic['_source']['title_terms'])
+            # if title_terms.issubset(set(tokenize_in_es(query, index))):
+            return topic
+    return None
+
+
+def test_search_all():
+    for tweet in ['Im in Stuwerviertel', "wien ist schön", "#leopoldstadt"]:
+        
+        # preprocess tweet
+        # remove urls
+        tweet = re.sub(r"(?:\@|https?\://)\S+", "", tweet)
+        tokens = tokenize_in_es(tweet)
+        query = ' '.join(f7(tokens))
+        print (query)
+
+        # query = "justin bieber will have a concert next tuesday"
+        results = search_all(query, threshold=0, explain=True, index=INDEX)
+        if results:
+            print (results['_score'])
+            print (results['_source']['keywords'])
+            print '\n'
+        # print results
+
+
+def search_duplicate_tweets(query, threshold=13, index=INDEX):
+    results = es.search(index=index, body={"query": {"match": {"tweet": query}}}, doc_type='tweets')['hits']
+    if results['max_score']:
+        if results['max_score'] > threshold:
+            return results['hits'][0]
+    return None
+
+
+def store_tweet(topic_id, tweet_text, index=INDEX):
+    es.index(index=index, doc_type='tweets', id=topic_id,
+             body={'tweet': tweet_text})
+
+
+def f7(seq):
+    '''
+    Order sequence to remove duplicates from tweets preserving order
+    '''
+    if len(seq) > 1:
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
+    else:
+        return seq
+
+
+class TopicListener(StreamListener):
+    '''
+    Overrides Tweepy class for Twitter Streaming API
+    '''
+
+    def on_status(self, status):
+        author = status.user.screen_name
+        # ignore retweets
+        if not hasattr(status,'retweeted_status') and status.in_reply_to_status_id == None:
+            text = status.text.replace('\n', '')
+            text = ' '.join([author, text])
+            report = text
+            if status.entities[u'user_mentions']:
+                mentions = ' '.join([entity[u'name'] for entity in status.entities[u'user_mentions']])
+                report += '\nMentions: ' + mentions
+                text = ' '.join([text, mentions])
+            if status.entities[u'urls']:
+                report += '\nURL'
+            if [u'media'] in status.entities.keys():
+                report += '\nMEDIA'
+
+            # preprocess tweet
+            # remove urls
+            text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+            if text:
+                tokens = tokenize_in_es(text)
+                if tokens:
+                    # print(tokens)
+                    query = ' '.join(f7(tokens))
+                    # print(query)
+                    # query elastic search
+                    results = search_all(query=query, threshold=THRESHOLD)
+                    if results:
+                        # check duplicates
+                        duplicates = search_duplicate_tweets(query=query)
+                        if not duplicates:
+
+                            # report tweet
+                            print ('Tweet:', report)
+                            # sent to ES
+                            print ('Query:', query)
+                            print (results['_score'])
+                            topic = results['_source']['keywords']
+                            print (topic)
+
+
+                            twitter_client.update_status(topic + ' https://twitter.com/%s/status/%s' % (author, status.id))
+                            
+                            # store tweets that have been reported to ES
+                            topid = results['_id']
+                            store_tweet(topid, query)
+                            
+                            print ('\n')
+
+        return True
+
+
+    def on_error(self, status_code):
+      print (status_code, 'error code')
+
+
+def stream_tweets():
+    '''
+    Connect to Twitter API and fetch relevant tweets from the stream
+    '''
+    listener = TopicListener()
+
+    # start streaming
+    while True:
+        try:
+            stream = Stream(auth_handler, listener)
+            print ('Listening...')
+            stream.sample(languages=['de'])
+        except Exception as e:
+            # reconnect on exceptions
+            print (e)
+            continue
+
+
+if __name__ == '__main__':
+    # test_search_all()
+    stream_tweets()
---
+++ index_leopoldtstadt.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+'''
+svakulenko
+7 Aug 2017
+'''
+from elasticsearch import Elasticsearch
+
+from conceptualization import lotus_recursive_call, loop_concept_expansion
+from topic2wiki import get_wiki_pages
+from search_google import search_google
+from scrape_duckduckgo import get_relevant_article
+
+
+# SEED = 'Leopoldstadt'
+# SEEDS = ['Österreich', 'Wien', 'Leopoldstadt', 'Stuwerviertel']
+SEEDS = ['Leopoldstadt', 'Stuwerviertel']
+
+
+mapping = {
+      'settings': {
+        # just one shard, no replicas for testing
+        'number_of_shards': 1,
+        'number_of_replicas': 0,
+      },
+      'mappings': {
+        'topics': {
+          'properties': {
+            'keywords': {'type': 'text', 'analyzer': 'german'},
+            'search_snippets': {'type': 'text', 'analyzer': 'german'},
+          }
+        },
+        'tweets': {
+          'properties': {
+            'tweet': {'type': 'text', 'analyzer': 'german'},
+          }
+        }
+      }
+    }
+
+def get_LODaLot(keywords='Leopoldstadt'):
+    concepts = lotus_recursive_call(keywords, filter_ns=False, size=10)
+    if concepts:
+        for concept_uris in concepts:
+            print (concept_uris)
+            concepts, descriptions = loop_concept_expansion(concept_uris)
+            print (concepts)
+            for hop in descriptions:
+                for description in hop:
+                    print (description)
+
+
+def get_wiki(keywords='Leopoldstadt'):
+    wiki = get_wiki_pages(keywords)
+    if wiki:
+        print (wiki[2])
+
+
+def get_google_snippets(keywords='Stuwerviertel'):
+    print (search_google(keywords))
+
+
+def load_topics_in_ES(topics, index_name='communidata', wiki=True, google=True, duckduck=False):
+    es = Elasticsearch()
+
+    # reset index
+    try:
+        es.indices.delete(index=index_name)
+        es.indices.create(index=index_name, body=mapping)
+    except Exception as e:
+        print (e)
+
+    for i, topic in enumerate(topics):
+
+        doc = {'keywords': topic}
+
+        if wiki:
+            # query expansion with Wikipedia articles
+            wiki_result = get_wiki_pages(topic)
+            if wiki_result:
+                wiki_title, wiki_summary, wiki_content = wiki_result
+                print (wiki_title)
+                doc['wiki_title'] = wiki_title
+                doc['wiki_summary'] = wiki_summary
+                doc['wiki_content'] = wiki_content
+
+        if google:
+            google_result = search_google(topic)
+            if google_result:
+                doc['search_snippets'] = google_result
+
+        if duckduck:
+            duckduck_result = get_relevant_article(topic)
+            if duckduck_result:
+                doc['web_page'] = duckduck_result
+
+        es.index(index=index_name, doc_type='topic', id=i,
+                 body=doc)
+
+
+if __name__ == '__main__':
+    load_topics_in_ES(SEEDS)
